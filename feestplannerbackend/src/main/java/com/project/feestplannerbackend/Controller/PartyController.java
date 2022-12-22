5a68c9c5965acc730359d6c501f9f06467aa5755
MiesvanGogh <93597938+MiesvanGogh@users.noreply.github.com>
2022-12-22 14:46:20 +0100

    sonar toegevoegd

--- feestplannerbackend/src/main/java/com/project/feestplannerbackend/Controller/PartyController.java
+++ feestplannerbackend/src/main/java/com/project/feestplannerbackend/Controller/PartyController.java
@@ -5,6 +5,7 @@ import com.project.feestplannerbackend.Repository.PartyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
--- feestplannerbackend/src/main/java/com/project/feestplannerbackend/Repository/PartyRepository.java
+++ feestplannerbackend/src/main/java/com/project/feestplannerbackend/Repository/PartyRepository.java
@@ -1,9 +1,14 @@
 package com.project.feestplannerbackend.Repository;
 
 import com.project.feestplannerbackend.Model.Party;
+import com.project.feestplannerbackend.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface PartyRepository extends JpaRepository<Party, Integer> {
+
 }
